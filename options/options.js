a845b91e31f463500cbb9972c0263b7fea92118c
Crowdbotics <engineering@crowdbotics.com>
2023-01-23 00:18:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "misty-bonus-dev-58641",
+  url: "https://misty-bonus-dev-58641.botics.co",
+  api: "https://misty-bonus-dev-58641.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
